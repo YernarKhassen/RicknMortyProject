1ef0287da46865b8e38b93cf4892f5c4f9145ee0
YernarKhassen <aseeayou@mail.ru>
2023-01-06 01:54:07 +0600

    Created Project

--- RicknMortyProject/Controllers/RMEpisodesViewController.swift
+++ RicknMortyProject/Controllers/RMEpisodesViewController.swift
@@ -11,7 +11,9 @@ class RMEpisodesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        view.backgroundColor = .systemBackground
+        title = "Episodes"
     }
 
 }
--- RicknMortyProject/Controllers/RMTabBarController.swift
+++ RicknMortyProject/Controllers/RMTabBarController.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+import UIKit
+
+final class RMTabBarViewController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupTabs()
+    }
+    
+    func setupTabs(){
+        let charVC = RMCharacterViewController()
+        let epiVC = RMEpisodesViewController()
+        let locVC = RMLocationsViewController()
+        let settingsVC = RMSettingsViewController()
+        
+        charVC.navigationItem.largeTitleDisplayMode = .automatic
+        epiVC.navigationItem.largeTitleDisplayMode = .automatic
+        locVC.navigationItem.largeTitleDisplayMode = .automatic
+        settingsVC.navigationItem.largeTitleDisplayMode = .automatic
+        
+        let nav1 = UINavigationController(rootViewController: charVC)
+        let nav2 = UINavigationController(rootViewController: epiVC)
+        let nav3 = UINavigationController(rootViewController: locVC)
+        let nav4 = UINavigationController(rootViewController: settingsVC)
+        
+        for nav in [nav1, nav2, nav3, nav4] {
+            nav.navigationBar.prefersLargeTitles = true
+        }
+        
+        nav1.tabBarItem = UITabBarItem(title: "Characters", image: UIImage(systemName: "person"), tag: 1)
+        nav2.tabBarItem = UITabBarItem(title: "Episodes", image: UIImage(systemName: "tv"), tag: 2)
+        nav3.tabBarItem = UITabBarItem(title: "Locations", image: UIImage(systemName: "globe"), tag: 3)
+        nav4.tabBarItem = UITabBarItem(title: "Settings", image: UIImage(systemName: "gearshape"), tag: 4)
+        
+        setViewControllers([nav1, nav2, nav3, nav4], animated: true)
+    }
+    
+}
